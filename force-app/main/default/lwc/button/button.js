518b8e2172d2f58fd09e30740776902522061581
Chetan17112 <chetanpmarwade@gmail.com>
2025-06-11 15:14:32 +0530

    1.36

--- force-app/main/default/lwc/button/button.js
+++ force-app/main/default/lwc/button/button.js
@@ -2,9 +2,13 @@ import { LightningElement } from 'lwc';
 
 export default class Button extends LightningElement {
     shows = false;
+    show = false;
 
     toggleView(){
         //this.shows = (this.shows === true) ? false : true;
         this.shows = !this.shows;
+    }
+    goodMView(){
+        this.show = !this.show;
     }
 }
