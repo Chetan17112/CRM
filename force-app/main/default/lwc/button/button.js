f545709aa3d4cb54786d620c6a7678f2b710e573
Chetan17112 <chetanpmarwade@gmail.com>
2025-06-11 16:19:04 +0530

    1.37

--- force-app/main/default/lwc/button/button.js
+++ force-app/main/default/lwc/button/button.js
@@ -3,12 +3,18 @@ import { LightningElement } from 'lwc';
 export default class Button extends LightningElement {
     shows = false;
     show = false;
-
+    classvisibility_variable = "slds-hide";
     toggleView(){
         //this.shows = (this.shows === true) ? false : true;
         this.shows = !this.shows;
     }
     goodMView(){
         this.show = !this.show;
+    }
+    classvisibility(){
+        if(this.classvisibility_variable == "slds-hide")
+        this.classvisibility_variable = "";
+        else
+        this.classvisibility_variable = "slds-hide";
     }
 }
