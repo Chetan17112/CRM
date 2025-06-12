d970c2511a572a6e33c8aa820df9e85967c82670
Chetan17112 <chetanpmarwade@gmail.com>
2025-06-12 19:20:53 +0530

    1.4

--- force-app/main/default/lwc/detailForm/detailForm.js
+++ force-app/main/default/lwc/detailForm/detailForm.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class DetailForm extends LightningElement {}
+export default class DetailForm extends LightningElement {
+    view = "";
+    ToggleView(){
+        this.view = this.view === '' ?'slds-hide slds-box':'';
+    }
+    constructor(){
+        super();
+        console.log("parent constructor running:")
+    }
+    connectedCallback(){
+        console.log("parent connectedCallback running:")
+    }
+    renderedCallback(){
+        console.log("parent renderedCallback running:")
+    }
+}
--- force-app/main/default/lwc/detailsformchildComponent/detailsformchildComponent.js
+++ force-app/main/default/lwc/detailsformchildComponent/detailsformchildComponent.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
 
-export default class DetailsformchildComponent extends LightningElement {}
+export default class DetailsformchildComponent extends LightningElement {
+    
+    constructor(){
+        super();
+        console.log("Child constructor running:")
+    }
+    connectedCallback(){
+        console.log("Child connectedCallback running:")
+    }
+    renderedCallback(){
+        console.log("Chil renderedCallback running:")
+    }
+}
